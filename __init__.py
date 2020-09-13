9c88e5445b2008fbdefc65832a703d680b65f2ef
Ray Yang <ruyang@linkedin.com>
2020-09-13 15:52:38 -0700

    Avoid adding duplicate questions

--- __init__.py
+++ __init__.py
@@ -14,14 +14,23 @@ def add_note():
     col = mw.col
     did = col.decks.id_for_name("test")
     m = col.models.byName("cc Chinese")
-    mid = m['id']
-    showInfo("deck id for the deck test: {}. Model id for cc Chinese: {}".format(did, mid))
+    # mid = m['id']
+    # showInfo("deck id for the deck test: {}. Model id for cc Chinese: {}".format(did, mid))
     col.models.setCurrent(m)
     n = col.newNote()
+    test_simplified = "测试"
     n['Pinyin'] = "ce4 shi4"
-    n['Simplified'] = "测试"
+    simplified_field_name = "Simplified"
+    n[simplified_field_name] = test_simplified
     n['English'] = "test"
-    col.add_note(n, did)
+
+    # showInfo(deck.keys())
+    node_ids = col.find_notes("{}:{}".format(simplified_field_name, test_simplified))
+    if node_ids:
+        showInfo("The note with the question {} already exists".format(test_simplified))
+    else:
+        col.add_note(n, did)
+        showInfo("Added a note with the question {}.".format(test_simplified))
 
 
 def show_card_count():
